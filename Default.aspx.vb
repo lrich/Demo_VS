3144b0a215b7e480ab0afbeb56d4c429565b21e6
lrich <leoncrich@gmail.com>
2010-04-16 10:35:00 -0400

    Added page load to vb file

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,4 +2,7 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Write("hiya World!")
+    End Sub
 End Class
